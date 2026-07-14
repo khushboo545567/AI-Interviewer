447345d5906d8186946742721b587ae52bf5721d
khushboo545567 <khushbooyadav6266763530@gmail.com>
2026-07-14 14:23:47 +0530

    origin updated

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ai-interviewer-1client-1mbl.onrender.com",
     credentials: true,
   }),
 );
