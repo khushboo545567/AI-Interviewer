19275aad9065cabbd7eb194a483fb47deeaa14e0
khushboo545567 <khushbooyadav6266763530@gmail.com>
2026-07-14 14:36:56 +0530

    cookie settings changes

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -15,7 +15,7 @@ const googleAuth = async (req, res) => {
     res.cookie("token", jwttoken, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.status(200).json(user);
